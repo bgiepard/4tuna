5d52a0d80fa3d1cad0ff23b9ac87b78659d3c8ac
Bartomiej <b.giepard@donedeliveries.com>
2024-10-19 11:16:46 +0200

    complete game 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { useGameContext } from './gameContext';
 
 const App = () => {
   const { gameInfo } = useGameContext();
+  const [currentScreen, setCurrentScreen] = useState('home');
+
   const [roundChange, setRoundChange] = useState(false);
 
   const prevRoundRef = useRef();
@@ -26,41 +28,54 @@ const App = () => {
 
   return (
     <div className="h-full bg-gradient-to-b from-blue-500 to-blue-800 py-1 mx-auto flex flex-col max-w-[800px]">
-      {roundChange ? (
-        <div className="h-full ">
-          {gameInfo.round > gameInfo.maxRounds ? (
-            <div className="p-4">
-              <h1 className="text-center text-white">KONIE GRY</h1>
-              <ul className="flex flex-col mx-5">
-                {gameInfo.players
-                  .sort((a, b) => b.total - a.total)
-                  .map((player) => {
-                    return (
-                      <div key={player.name}>
-                        <span>{player.name}</span> - <span>{player.total}</span>
-                      </div>
-                    );
-                  })}
-              </ul>
+      {currentScreen === 'home' && (
+        <div className="flex flex-col">
+          <button onClick={() => setCurrentScreen('game')}>Zagraj solo</button>
+          <button>Dołącz do gry</button>
+          <button>Wylosuj przeciwnika</button>
+          <button>Stwórz grę</button>
+        </div>
+      )}
+      {currentScreen === 'game' && (
+        <>
+          {roundChange ? (
+            <div className="h-full ">
+              {gameInfo.round > gameInfo.maxRounds ? (
+                <div className="p-4">
+                  <h1 className="text-center text-white">KONIE GRY</h1>
+                  <ul className="flex flex-col mx-5">
+                    {gameInfo.players
+                      .sort((a, b) => b.total - a.total)
+                      .map((player) => {
+                        return (
+                          <div key={player.name}>
+                            <span>{player.name}</span> -{' '}
+                            <span>{player.total}</span>
+                          </div>
+                        );
+                      })}
+                  </ul>
+                </div>
+              ) : (
+                <div className="h-full flex flex-col items-center justify-center">
+                  <h1 className="text-white">Zmiana rundy</h1>
+                </div>
+              )}
             </div>
           ) : (
-            <div className="h-full flex flex-col items-center justify-center">
-              <h1 className="text-white">Zmiana rundy</h1>
-            </div>
+            <>
+              <Phrase />
+
+              <div className="flex flex-col items-center justify-center flex-grow ">
+                <PieChart />
+                <Buttons />
+              </div>
+
+              <PlayersInfo />
+
+              <Keyboard />
+            </>
           )}
-        </div>
-      ) : (
-        <>
-          <Phrase />
-
-          <div className="flex flex-col items-center justify-center flex-grow ">
-            <PieChart />
-            <Buttons />
-          </div>
-
-          <PlayersInfo />
-
-          <Keyboard />
         </>
       )}
     </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { GameContextProvider } from "./gameContext";
+import { GameContextProvider } from './gameContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
