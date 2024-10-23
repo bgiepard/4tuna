814e3a005d06d20703a40db19ad06bfdb2c5d8c3
Bartomiej <b.giepard@donedeliveries.com>
2024-10-23 14:06:17 +0200

    fr

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://4tuna.pl:3000'); // Adres Twojego serwera
+const socket = io('https://4tuna.pl'); // Adres Twojego serwera
 
 export default socket;
