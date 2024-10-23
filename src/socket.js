eeb6bd11b8b1697338572db0e194d3cbaa5e2d40
Bartomiej <b.giepard@donedeliveries.com>
2024-10-23 14:38:01 +0200

    fre

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,15 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://4tuna.pl'); // Adres Twojego serwera
+const socket = io('https://4tuna.pl', {
+  transports: ['websocket', 'polling'],
+});
+
+socket.on('connect', () => {
+  console.log('Connected to WebSocket');
+});
+
+socket.on('connect_error', (err) => {
+  console.error('WebSocket connection error:', err);
+});
 
 export default socket;
