967af0a304ad0e59aa1ec73f98a275a8cb4c4af9
Bartomiej <b.giepard@donedeliveries.com>
2024-10-26 16:42:11 +0200

    fixes

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,8 @@ import { io } from 'socket.io-client';
 
 // Inicjalizacja Socket.io bezpośrednio z bieżącej domeny
 const socket = io({
-  path: '/socket.io',
+  // path: '/socket.io',
+  path: 'https://4tuna.pl//socket.io',
   transports: ['websocket', 'polling'],
   secure: true, // Użyj `true` jeśli frontend jest na HTTPS
   rejectUnauthorized: false, // Użyj tylko do debugowania, NIE w produkcji
@@ -22,9 +23,3 @@ socket.on('disconnect', () => {
 });
 
 export default socket;
-
-// import { io } from 'socket.io-client';
-//
-// const socket = io('http://localhost:5000'); // Adres Twojego serwera
-//
-// export default socket;
